b40615835efa45a0072d1581840ae912662efd45
butschster <butschster@gmail.com>
2013-10-07 19:46:50 +0400

    Исправлена ошибка с добавлением query string

--- cms/media/js/backend.js
+++ cms/media/js/backend.js
@@ -394,7 +394,7 @@ cms.ui.add('btn-confirm', function() {
 		openEffect	: 'none',
 		closeEffect	: 'none',
 		beforeLoad: function() {
-			this.href += '?type=iframe';
+			this.href = updateQueryStringParameter(this.href, 'type', 'iframe');
 			this.title = $(this.element).html();
 		},
 		helpers : {
@@ -513,6 +513,17 @@ var Api = {
 	},
 	response: function() {
 		return this._response;
+	}
+}
+
+function updateQueryStringParameter(uri, key, value) {
+	var re = new RegExp("([?|&])" + key + "=.*?(&|$)", "i");
+	separator = uri.indexOf('?') !== -1 ? "&" : "?";
+	if (uri.match(re)) {
+		return uri.replace(re, '$1' + key + "=" + value + '$2');
+	}
+	else {
+		return uri + separator + key + "=" + value;
 	}
 }
 
