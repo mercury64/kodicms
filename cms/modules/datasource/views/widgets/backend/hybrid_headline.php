8b72447373da4e46f285a1ac8f1715485600cc89
butschster <butschster@gmail.com>
2013-10-10 11:29:04 +0400

    fix

--- cms/modules/datasource/views/widgets/backend/hybrid_headline.php
+++ cms/modules/datasource/views/widgets/backend/hybrid_headline.php
@@ -7,28 +7,6 @@
 </div>
 <div class="widget-content">
 	<div class="control-group">
-		<label class="control-label" for="list_offset"><?php echo __('Number of documents to omit'); ?></label>
-		<div class="controls">
-			<?php
-			echo Form::input( 'list_offset', $widget->list_offset, array(
-				'class' => 'input-small', 'id' => 'list_offset'
-			) );
-			?>
-		</div>
-	</div>
-
-	<div class="control-group">
-		<label class="control-label" for="list_size"><?php echo __('Number of documents per page'); ?></label>
-		<div class="controls">
-			<?php
-			echo Form::input( 'list_size', $widget->list_size, array(
-				'class' => 'input-small', 'id' => 'list_size'
-			) );
-			?>
-		</div>
-	</div>
-
-	<div class="control-group">
 		<label class="control-label" for="doc_uri"><?php echo __('Document page (URI)'); ?></label>
 		<div class="controls">
 			<?php
